30099cc28525a1a7bb73d7aa4e712d0bdd42174f
David Lopes <david.mlopes15@gmail.com>
2024-10-30 21:57:39 +0000

    feat: position dark mode button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import DarkModeToggle from "@/components/DarkModeToggle";
 export default function Home() {
   return (
     <div>
-      <DarkModeToggle />
+      <div className="absolute bottom-2 right-2 sm:bottom-4 sm:right-4">
+        <DarkModeToggle />
+      </div>
     </div>
   );
 }
